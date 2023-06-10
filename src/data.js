b4ce51413d14e123b1e84ea6b50ae7c7ca596cc0
Caroline Thostrup <caro342h@stud.kea.dk>
2023-06-10 13:47:23 +0200

    mock data

---
+++ src/data.js
@@ -0,0 +1,8 @@
+[
+  { id: 0, question: "Muninn was a ____ bird who flew around the world to collect information for Odin. Fill in the blank.", type: "FIB", correctAnswer: "raven" },
+  { id: 1, question: "Which of the following is not a primary colour", choices: ["Red", "Blue", "Green", "Yellow"], type: "MCQs", correctAnswer: "Green" },
+  { id: 2, question: "What is the capital of Australia?", choices: ["Sydney", "Melbourne", "Canberra", "Perth"], type: "MCQs", correctAnswer: "Canberra" },
+  { id: 3, question: "What is the capital of France?", choices: ["Paris", "London", "Berlin", "Madrid"], type: "MCQs", correctAnswer: "Paris" },
+  { id: 4, question: "Who painted the Mona Lisa?", choices: ["Pablo Picasso", "Leonardo da Vinci", "Vincent van Gogh", "Claude Monet"], type: "MCQs", correctAnswer: "Leonardo da Vinci" },
+  { id: 5, question: "What is the best programming language?", choices: ["HTML", "CSS", "JavaScript", "React"], type: "MCQs", correctAnswer: "React" },
+];
