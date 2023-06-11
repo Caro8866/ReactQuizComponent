6a3b1d051df088fb0912ae9ac01bcbf9800416ff
Caroline Thostrup <caro342h@stud.kea.dk>
2023-06-11 17:50:16 +0200

    switch question order to allow for multiplechoice

--- src/data.js
+++ src/data.js
@@ -1,7 +1,7 @@
 export const jsonData = {
   questions: [
-    { id: 0, question: "Muninn was a ____ bird who flew around the world to collect information for Odin. Fill in the blank.", type: "FIB", correctAnswer: "raven" },
-    { id: 1, question: "Which of the following is not a primary colour", choices: ["Red", "Blue", "Green", "Yellow"], type: "MCQs", correctAnswer: "Green" },
+    { id: 0, question: "Which of the following is not a primary colour", choices: ["Red", "Blue", "Green", "Yellow"], type: "MCQs", correctAnswer: "Green" },
+    { id: 1, question: "Muninn was a ____ bird who flew around the world to collect information for Odin. Fill in the blank.", type: "FIB", correctAnswer: "raven" },
     { id: 2, question: "What is the capital of Australia?", choices: ["Sydney", "Melbourne", "Canberra", "Perth"], type: "MCQs", correctAnswer: "Canberra" },
     { id: 3, question: "What is the capital of France?", choices: ["Paris", "London", "Berlin", "Madrid"], type: "MCQs", correctAnswer: "Paris" },
     { id: 4, question: "Who painted the Mona Lisa?", choices: ["Pablo Picasso", "Leonardo da Vinci", "Vincent van Gogh", "Claude Monet"], type: "MCQs", correctAnswer: "Leonardo da Vinci" },
