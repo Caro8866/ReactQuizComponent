d9ef7772299d755db245763e97a2f32f68d7dd1d
Caroline Thostrup <caro342h@stud.kea.dk>
2023-06-12 21:46:05 +0200

    add filllinTheBlank and fix a lot of bugs

--- src/Components/quiz/Quiz.jsx
+++ src/Components/quiz/Quiz.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import PropTypes from "prop-types";
 import Timer from "../Timer/Timer";
 import MultipleChoice from "../MultipleChoice/MultipleChoice";
+import FillInTheBlank from "../FillInTheBlank/FillInTheBlank";
 
 const resultInitState = {
   score: 0,
@@ -17,6 +18,7 @@ function Quiz({ questions }) {
   const [result, setResult] = useState(resultInitState);
   const [showResult, setShowResult] = useState(false);
   const [displayTimer, setDisplayTimer] = useState(true);
+  const [FITBAnswer, setFITBAnswer] = useState("");
 
   const { question, choices, correctAnswer, type } = questions[currentQuestion];
 
@@ -29,9 +31,20 @@ function Quiz({ questions }) {
 
     setResult((previousResult) => {
       const finalAnswer = selectedAnswerIndex !== null && choices[selectedAnswerIndex] === correctAnswer;
-      const scoreChange = finalAnswer ? 10 : 0;
-      const correctChange = finalAnswer ? 1 : 0;
-      const incorrectChange = finalAnswer ? 0 : 1;
+
+      let scoreChange = 0;
+      let correctChange = 0;
+      let incorrectChange = 0;
+
+      if (type === "MC") {
+        scoreChange = finalAnswer ? 10 : 0;
+        correctChange = finalAnswer ? 1 : 0;
+        incorrectChange = finalAnswer ? 0 : 1;
+      } else if (type === "FITB") {
+        scoreChange = answer ? 10 : 0;
+        correctChange = answer ? 1 : 0;
+        incorrectChange = answer ? 0 : 1;
+      }
 
       return {
         score: previousResult.score + scoreChange,
@@ -43,6 +56,8 @@ function Quiz({ questions }) {
     if (currentQuestion !== questions.length - 1) {
       setCurrentQuestion(currentQuestion + 1);
       setSelectedAnswerIndex(null);
+      setFITBAnswer(""); // Reset FITBAnswer to an empty string
+      setAnswer(null); // Reset answer to null
       setTimeout(() => {
         setDisplayTimer(true);
       }, 100); // Delay to allow the component to re-render before starting the timer
@@ -56,6 +71,8 @@ function Quiz({ questions }) {
     setShowResult(false);
     setSelectedAnswerIndex(null);
     setCurrentQuestion(0);
+    setFITBAnswer(""); // Reset FITBAnswer to an empty string
+    setAnswer(null); // Reset answer to null
     setTimeout(() => {
       setDisplayTimer(true);
     }, 100); // Delay to allow the component to re-render before starting the timer
@@ -66,21 +83,40 @@ function Quiz({ questions }) {
     onNextQuestionClick(false);
   };
 
+  const handleFITBAnswerChange = (e) => {
+    const userAnswer = e.target.value; // Get the user input
+    setFITBAnswer(userAnswer);
+    const lowercaseUserAnswer = userAnswer.toLowerCase(); // Convert the user input to lowercase
+    const lowercaseCorrectAnswer = correctAnswer.toLowerCase(); // Convert the correct answer to lowercase
+
+    const isAnswerCorrect = lowercaseUserAnswer === lowercaseCorrectAnswer;
+    setAnswer(isAnswerCorrect);
+  };
+
+  const displayAnswers = () => {
+    if (type === "MC") {
+      return <MultipleChoice choices={choices} selectAnswer={selectAnswer} selectedAnswerIndex={selectedAnswerIndex} />;
+    } else if (type === "FITB") {
+      return <FillInTheBlank value={FITBAnswer} handleAnswerChange={handleFITBAnswerChange} />;
+    }
+  };
+
   return (
     <div className={styles.quizContainer}>
       {!showResult ? (
         <>
           {displayTimer && <Timer duration={3} onTimeUp={handleTimeOut} />}
-          <header className={styles.questionNum}>
+          <header className={styles.header}>
             <span className={styles.currentQuestionNum}>{currentQuestion + 1}</span>/<span className={styles.totalQuestionsNum}>{questions.length}</span>
+            <h2 className={styles.questionText}>{question}</h2>
           </header>
-          {type === "MC" ? <MultipleChoice choices={choices} question={question} selectAnswer={selectAnswer} selectedAnswerIndex={selectedAnswerIndex} /> : null}
+          {displayAnswers()}
           <footer className={styles.quizFooter}>
             <button
               onClick={() => {
                 onNextQuestionClick(answer);
               }}
-              disabled={selectedAnswerIndex === null}
+              disabled={selectedAnswerIndex === null && !FITBAnswer}
             >
               {currentQuestion === questions.length - 1 ? "Finish!" : "Next Question..."}
             </button>
