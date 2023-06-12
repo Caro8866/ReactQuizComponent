0c51b2259d3a33c8414bea5906ddcd3cc04cf460
Caroline Thostrup <caro342h@stud.kea.dk>
2023-06-12 21:45:52 +0200

    add filllintheblnk question data

--- src/data.js
+++ src/data.js
@@ -1,9 +1,10 @@
 export const jsonData = {
   questions: [
     { id: 0, question: "Which of the following is not a primary colour", choices: ["Red", "Blue", "Green", "Yellow"], type: "MC", correctAnswer: "Green" },
-    { id: 1, question: "What is the capital of Australia?", choices: ["Sydney", "Melbourne", "Canberra", "Perth"], type: "MC", correctAnswer: "Canberra" },
-    { id: 2, question: "What is the capital of France?", choices: ["Paris", "London", "Berlin", "Madrid"], type: "MC", correctAnswer: "Paris" },
-    { id: 3, question: "Who painted the Mona Lisa?", choices: ["Pablo Picasso", "Leonardo da Vinci", "Vincent van Gogh", "Claude Monet"], type: "MC", correctAnswer: "Leonardo da Vinci" },
-    { id: 4, question: "What is the best programming language?", choices: ["HTML", "CSS", "JavaScript", "React"], type: "MC", correctAnswer: "React" },
+    { id: 1, question: "Muninn was a ____ bird who flew around the world to collect information for Odin. Fill in the blank.", type: "FITB", correctAnswer: "Raven" },
+    { id: 2, question: "What is the capital of Australia?", choices: ["Sydney", "Melbourne", "Canberra", "Perth"], type: "MC", correctAnswer: "Canberra" },
+    { id: 3, question: "What is the capital of France?", choices: ["Paris", "London", "Berlin", "Madrid"], type: "MC", correctAnswer: "Paris" },
+    { id: 4, question: "Who painted the Mona Lisa?", choices: ["Pablo Picasso", "Leonardo da Vinci", "Vincent van Gogh", "Claude Monet"], type: "MC", correctAnswer: "Leonardo da Vinci" },
+    { id: 5, question: "What is the best programming language?", choices: ["HTML", "CSS", "JavaScript", "React"], type: "MC", correctAnswer: "React" },
   ],
 };
